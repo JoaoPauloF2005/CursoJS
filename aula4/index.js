710ddf9fb44c2b1b60e5914f77f2191febeb17b9
joao <hypergamerfrr@gmail.com>
2023-04-20 16:20:43 -0300

    att

---
+++ aula4/index.js
@@ -0,0 +1,19 @@
+// Não podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+// Não pode começar o nome de uma variavel com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE LET
+
+// + - * /
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;  
+console.log(resultadoTriplicado);
+
+
